bc3b9023850d5cb4fcb5b8d4ade37515b46b0fe8
Naea Fairbrother <NaeaFairbrother@gmail.com>
2021-11-18 13:56:56 +0000

    Coroutines

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
 {
-    [SerializeField] List<Waypoint> path = new List<Waypoint>(); 
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float waitTime;
 
     void Start()
     {
-        
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        foreach(Waypoint waypoint in this.path)
+        {
+            this.transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitTime);
+        }  
     }
 }
