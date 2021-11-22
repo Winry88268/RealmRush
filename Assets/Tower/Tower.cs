2ec8b77f4906ba5625355a84324936d07f7e84a7
Naea Fairbrother <NaeaFairbrother@gmail.com>
2021-11-22 01:56:51 +0000

    Build Delay

--- Assets/Tower/Tower.cs
+++ Assets/Tower/Tower.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] int towerCost = 75;
+    [SerializeField] float buildDelay = 1f;
+
+    void Start() 
+    {
+        StartCoroutine(Build());
+    }
 
     public bool CreateTower(Tower tower, Vector3 position)
     {
@@ -20,5 +26,27 @@ public class Tower : MonoBehaviour
         }
         
         return false;
+    }
+
+    IEnumerator Build()
+    {
+        foreach(Transform child in this.transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach(Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(false);
+            }
+        }
+
+        foreach(Transform child in this.transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(buildDelay);
+            foreach(Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(true);
+            }
+        }
     }
 }
